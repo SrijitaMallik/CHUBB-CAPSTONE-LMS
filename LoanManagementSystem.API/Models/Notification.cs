c21d6da2fc8bd34e113ece4eca8a38b653d9818e
SrijitaMallik <157941680+SrijitaMallik@users.noreply.github.com>
2026-01-07 17:42:16 +0530

    Update Notification.cs

--- LoanManagementSystem.API/Models/Notification.cs
+++ LoanManagementSystem.API/Models/Notification.cs
@@ -1,9 +1,10 @@
-﻿namespace LoanManagementSystem.API.Models
+namespace LoanManagementSystem.API.Models
 {
     public class Notification
     {
         public int NotificationId { get; set; }
         public int UserId { get; set; }
+        public string Title { get; set; } = "";  // Added
         public string Message { get; set; } = "";
         public bool IsRead { get; set; } = false;
         public DateTime CreatedAt { get; set; } = DateTime.Now;
